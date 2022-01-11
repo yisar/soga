92646ad58c6d93c2431aa09fdbcb5be12d41e38d
changhaozhao <changhaozhao@trip.com>
2022-01-11 12:37:54 +0800

    emmm

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use flexbox::FlexBox;
 
 fn main() {
     let mut root = FlexItem::flex_item_with_size(100.0,100.0);
-    let mut child = FlexItem::flex_item_new();
-    child.flex_item_set_width(40.0);
-    child.flex_item_set_height(40.0);
+    let mut child1 = FlexItem::flex_item_new();
+    child1.flex_item_set_width(40.0);
+    child1.flex_item_set_height(40.0);
+
+    let mut child2 = FlexItem::flex_item_with_size(20.0, 20.0);
     
-    root.flex_item_add(child);
+    root.flex_item_add(child1);
+    root.flex_item_add(child2);
 
     let mut layout = FlexBox::new();
     layout.layout_item(&root, 100.0, 100.0);
