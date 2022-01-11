3f53428c85600cb4fdfa6355e7b54f33fdff5da3
changhaozhao <changhaozhao@trip.com>
2022-01-11 15:38:46 +0800

    emmm

--- src/test.rs
+++ src/test.rs
@@ -2,19 +2,18 @@ pub mod flexbox;
 use flexbox::FlexBox;
 use flexbox::FlexItem;
 
-fn main() {
+fn grow() {
     let mut root = FlexItem::new(100.0, 240.0);
     let mut child1 = FlexItem::new(60.0, 30.0);
     let mut child2 = FlexItem::new(60.0, 0.0);
     let mut child3 = FlexItem::new(60.0, 0.0);
 
+    child1.set_grow(0);
+    child2.set_grow(1);
+    child3.set_grow(2);
+
 
     root.add(child1);
     root.add(child2);
-    root.add(child3)
-
-    let mut layout = FlexBox::new();
-    layout.layout(&mut root);
-
-    print!("{:#?}", root);
+    root.add(child3);
 }
