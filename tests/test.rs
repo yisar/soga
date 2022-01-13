cff7622ca884043d7847110c02f43d950530fb55
伊撒尔 <1533540012@qq.com>
2022-01-13 15:55:56 +0800

    add justify_content tests

--- tests/test.rs
+++ tests/test.rs
@@ -27,3 +27,23 @@ fn grow() {
     assert_eq!(root.children[1].frame, [0, 30, 60, 70]);
     assert_eq!(root.children[2].frame, [0, 100, 60, 140])
 }
+
+#[test]
+fn justify_content() {
+    let mut root = FlexItem::new(100, 300);
+
+    root.direction = "column".try_into().unwrap();
+    root.justify_content = "center".try_into().unwrap();
+
+    let mut child1 = FlexItem::new(50, 100);
+    let mut child2 = FlexItem::new(50, 100);
+
+    root.add(child1);
+    root.add(child2);
+
+    let mut flexbox = FlexBox::new();
+    flexbox.layout(&mut root);
+
+    assert_eq!(root.children[0].frame, [0, 50, 50, 100]);
+    assert_eq!(root.children[1].frame, [0, 150, 50, 100]);
+}
