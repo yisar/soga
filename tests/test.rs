93dc91cc7b1eaf692886ac94fa7ef4a264afc7ca
伊撒尔 <1533540012@qq.com>
2022-01-13 16:14:35 +0800

    add tests

--- tests/test.rs
+++ tests/test.rs
@@ -70,3 +70,25 @@ fn justify_content() {
     assert_eq!(root.children[0].frame, [0, 50, 50, 100]);
     assert_eq!(root.children[1].frame, [0, 150, 50, 100]);
 }
+
+#[test]
+fn align_items() {
+    let mut root = FlexItem::new(100, 10);
+
+    root.direction = "column".try_into().unwrap();
+
+    let mut child1 = FlexItem::new(50, 25);
+    let mut child2 = FlexItem::new(50, 25);
+    let mut child3 = FlexItem::new(50, 25);
+
+    root.add(child1);
+    root.add(child2);
+    root.add(child3);
+
+    let mut flexbox = FlexBox::new();
+    flexbox.layout(&mut root);
+
+    assert_eq!(root.children[0].frame, [25, 0, 50, 25]);
+    assert_eq!(root.children[1].frame, [25, 25, 50, 25]);
+    assert_eq!(root.children[2].frame, [25, 50, 50, 25])
+}
